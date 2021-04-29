0647e18b1909ee74566a8daa05445baf0401ee6c
IszalyZs <iszalyzs@gmail.com>
2021-04-29 12:13:17 +0200

    Ass project

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,32 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.*;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+
+        if (output == null || contacts == null)
+            try {
+                throw new IllegalAccessException();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+        File file = new File(output);
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                String name = entry.getKey();
+                String phoneNumber = entry.getValue();
+                String line = name + ": " + phoneNumber;
+                bufferedWriter.write(line + "\n");
+            }
+            bufferedWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
